d52e06fd91327753ca51f4e306cc5d83b89411bc
shadowabhii <abhishek44221@gmail.com>
2022-08-22 00:29:41 -0700

    first commit

---
+++ src/main/java/com/kafka/KAfkaPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KAfkaPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KAfkaPublisherApplication.class, args);
+	}
+
+}
